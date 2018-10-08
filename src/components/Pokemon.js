5b33df8e596283489a759a21f147d2d88dc06a79
Laura Domingo <laura.domingo.82@gmail.com>
2018-10-08 02:42:11 -0700

    pintada imagen y tipo

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -4,13 +4,15 @@ class Pokemon extends React.Component {
 
   render(){
 
-      const{name, id}= this.props.item;
+      const{name, id, sprites, types }= this.props.item;
     return (
-      <React.Fragment>
-          <h1>{name}</h1>
-          <p>{id}</p>
-        
-      </React.Fragment>
+      <div className="card">
+          <img className="photo-card" src={sprites.front_default} alt={name}/>
+          <p className="id-card" > ID /{id}</p>
+          <h1 className="tite-card">{name}</h1>
+          <p className="type-card">{types.map(pokemonType => <p>{pokemonType.type.name}</p>)}</p>
+
+      </div>
 
     );
   }
